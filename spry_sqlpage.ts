aaaf4c50641a45e8f89edc46975efed06156e5cd
Siju Joseph <siju-joseph@netspective.in>
2025-12-01 14:03:57 +0530

    Update CLI import paths in spry_runbook and spry_sqlpage to use version 0.13.2

--- spry_sqlpage.ts
+++ spry_sqlpage.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run -A --node-modules-dir=auto
 // Use `deno run -A --watch` in the shebang if you're contributing / developing Spry itself.
 
-import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/heads/main/lib/sqlpage/cli.ts";
+import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/tags/v0.13.2/lib/sqlpage/cli.ts";
 
 CLI.instance().run();
