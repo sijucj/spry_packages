ebc1c4f894a158531e96daf24ce4a573a905c242
Siju Joseph <siju-joseph@netspective.in>
2025-12-12 11:13:08 +0530

    fix: version addded

--- spry.ts
+++ spry.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run -A --node-modules-dir=auto
 // Use `deno run -A --watch` in the shebang if you're contributing / developing Spry itself.
 
-import { CLI } from "https://raw.githubusercontent.com/sijucj/spry_private/refs/heads/main/bin/spry.ts";
+import { CLI } from "https://raw.githubusercontent.com/sijucj/spry_private/refs/tags/v0.102.1/bin/spry.ts";
 
 await CLI({ defaultFiles: ["Spryfile.md"] }).parse(Deno.args);
