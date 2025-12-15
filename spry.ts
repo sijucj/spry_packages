d2d62391ff52e9bd3f9d8ac4dd2e2ba7634e6bb1
Siju Joseph <siju-joseph@netspective.in>
2025-12-15 13:18:32 +0530

    Release v0.106.0

--- spry.ts
+++ spry.ts
@@ -1,10 +1,6 @@
 #!/usr/bin/env -S deno run -A --node-modules-dir=auto
 // Use `deno run -A --watch` in the shebang if you're contributing / developing Spry itself.
 
-// import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/tags/v0.106.0/bin/spry.ts";
+import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/tags/v0.106.0/bin/spry.ts";
 
-// await CLI({ defaultFiles: ["Spryfile.md"] }).parse(Deno.args);
-
-import { CLI } from "https://raw.githubusercontent.com/programmablemd/spry/refs/tags/v0.106.0/lib/sqlpage/cli.ts";
-
-CLI.instance().run();
+await CLI({ defaultFiles: ["Spryfile.md"] }).parse(Deno.args);
